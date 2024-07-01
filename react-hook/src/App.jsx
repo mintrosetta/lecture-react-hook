19a07f7e4b20759808f0da29782e39976946a48e
Mint Rosetta <mint.rosetta2001@gmail.com>
2024-07-01 11:16:17 +0700

    useEffect

--- react-hook/src/App.jsx
+++ react-hook/src/App.jsx
@@ -1,10 +1,30 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [name, setName] = useState("Hello World");
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((users) => {
+        console.log(users);
+        setData(users);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users/" + count)
+      .then((res) => res.json())
+      .then((users) => {
+        console.log(users);
+        setData(users);
+      });
+  }, [count]);
 
   return (
     <>
@@ -16,7 +36,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>{name}</h1>
+      <button type='button' onClick={() => setName("EEE")}>Set name</button>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
